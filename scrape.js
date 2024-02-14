77f2cf580c1aea933d582b584d4e763549cb19df
apsasinka <aris4a.vima@gmail.com>
2024-02-14 13:51:23 +0300

    Adding a resolution selection function.

--- scrape.js
+++ scrape.js
@@ -8,6 +8,7 @@ async function uploadFile(page, filePath) {
     }
 
     try {
+        await page.waitForSelector('#dropBox > button', { visible: true });
         const fileInput = await page.$('body > input');
         await fileInput.uploadFile(filePath);
 
@@ -17,15 +18,48 @@ async function uploadFile(page, filePath) {
     }
 }
 
+async function selectResolution(page, resolution) {
+    try {
+        const resolutionActions = {
+            '2': async () => {
+                await page.waitForSelector('#actionBtns > div > button', { visible: true });
+                await page.click('#actionBtns > div > button');
+            },
+            '3': async () => {
+                await page.waitForSelector('#actionBtns > div > button', { visible: true });
+                await page.click('#content > div > div > div.col-sm-7 > div:nth-child(8) > form > div:nth-child(1) > div > button');
+                await page.click('#bs-select-1-2');
+                await page.click('#actionBtns > div > button');
+            },
+            '4': async () => {
+                await page.waitForSelector('#actionBtns > div > button', { visible: true });
+                await page.click('#content > div > div > div.col-sm-7 > div:nth-child(8) > form > div:nth-child(1) > div > button');
+                await page.click('#bs-select-1-3');
+                await page.click('#actionBtns > div > button');
+            },
+        };
+
+        const action = resolutionActions[resolution];
+        if (action) {
+            await action();
+        } else {
+            console.error('Неподдерживаемое разрешение:', resolution);
+        }
+    } catch (error) {
+        console.error('Произошла ошибка при выборе разрешения:', error);
+    }
+};
 
 (async () => {
     const url = process.env.URL;
     const filePath = process.env.FILEPATH;
+    const resolution = process.env.RESOLUTION;
 
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto(url, { timeout: 60000 });
-    await page.setViewport({ width: 1500, height: 1000 });
+    await page.setViewport({ width: 1200, height: 1000 });
+    await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 60000 });
 
     await uploadFile(page, filePath);
+    await selectResolution(page, resolution);
 })();
