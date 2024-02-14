b2cd38deb9757a1325696c12872c63e8812f564e
apsasinka <aris4a.vima@gmail.com>
2024-02-14 14:13:38 +0300

    Added error detection when the resource server is busy.

--- scrape.js
+++ scrape.js
@@ -24,18 +24,21 @@ async function selectResolution(page, resolution) {
             '2': async () => {
                 await page.waitForSelector('#actionBtns > div > button', { visible: true });
                 await page.click('#actionBtns > div > button');
+                console.log('Загружено разрешение 2х');
             },
             '3': async () => {
                 await page.waitForSelector('#actionBtns > div > button', { visible: true });
                 await page.click('#content > div > div > div.col-sm-7 > div:nth-child(8) > form > div:nth-child(1) > div > button');
                 await page.click('#bs-select-1-2');
                 await page.click('#actionBtns > div > button');
+                console.log('Загружено разрешение 3х');
             },
             '4': async () => {
                 await page.waitForSelector('#actionBtns > div > button', { visible: true });
                 await page.click('#content > div > div > div.col-sm-7 > div:nth-child(8) > form > div:nth-child(1) > div > button');
                 await page.click('#bs-select-1-3');
                 await page.click('#actionBtns > div > button');
+                console.log('Загружено разрешение 4х');
             },
         };
 
@@ -50,6 +53,54 @@ async function selectResolution(page, resolution) {
     }
 };
 
+async function handleServerOverload(page, filePath, resolution, browser) {
+    try {
+        await page.waitForSelector('#content > div > div > div.text-danger.text-center.font-weight-bold.font-size-3', { visible: true });
+        console.error('Ошибка: Сервер ресурса перегружен.');
+
+        const actions = {
+            '1': async () => {
+                console.log('Перезагрузка...');
+                await page.click('#start_over');
+                await uploadFile(page, filePath);
+                await selectResolution(page, resolution);
+                await handleServerOverload(page);
+            },
+            '2': async () => {
+                console.log('Завершение процесса...');
+                await browser.close();
+            }
+        };
+
+        let action;
+        do {
+            const answer = await askUser('Выберите действие: 1 - Перезагрузить, 2 - Завершить');
+            action = actions[answer];
+
+            if (!action) {
+                console.error('Неправильный выбор.');
+            }
+        } while (!action);
+
+        await action();
+    } catch (error) {
+        console.error('Произошла ошибка при обработке перегрузки сервера:', error);
+    }
+};
+
+async function askUser(question) {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    return new Promise(resolve => {
+        readline.question(question + ' ', answer => {
+            readline.close();
+            resolve(answer);
+        });
+    });
+};
 (async () => {
     const url = process.env.URL;
     const filePath = process.env.FILEPATH;
@@ -62,4 +113,5 @@ async function selectResolution(page, resolution) {
 
     await uploadFile(page, filePath);
     await selectResolution(page, resolution);
+    await handleServerOverload(page, filePath, resolution, browser);
 })();
